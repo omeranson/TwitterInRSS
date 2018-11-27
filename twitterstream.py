b9fc23e626d6301504bb4885266decfaa7c8195d
Saggi Mizrahi <saggi@mizrahi.cc>
2018-11-27 15:49:41 +0200

    fix pep8 errors

--- twitterstream.py
+++ twitterstream.py
@@ -15,7 +15,11 @@ access_token_secret = os.environ.get("TIS_TOKEN_SECRET", "")
 uuid_namespace = uuid.UUID('e8cd8531-a8df-4b41-a249-479d15252349')
 
 t = twitter.Twitter(
-    auth=twitter.OAuth(access_token_key, access_token_secret, api_key, api_secret))
+    auth=twitter.OAuth(access_token_key,
+                       access_token_secret,
+                       api_key,
+                       api_secret))
+
 
 class RateLimiter(object):
     def __init__(self, max_rate=3, time_unit=1):
@@ -33,8 +37,10 @@ class RateLimiter(object):
         self.deque.append(time.time())
         return False
 
+
 cached_output = None
 rate_limiter = RateLimiter(2, 10)
+
 
 def create_rss_feed_xml():
     tweets = t.statuses.home_timeline()
@@ -49,7 +55,7 @@ def create_rss_feed_xml():
         fe = fg.add_entry()
         fe.id('https://twitter.com/i/status/%s' % (tweet['id_str']))
         fe.title('%s (@%s)' % (tweet['user']['name'],
-                                      tweet['user']['screen_name']))
+                               tweet['user']['screen_name']))
         fe.link(href='https://twitter.com/i/status/%s' % (tweet['id_str']))
         fe.description(tweet['text'])
         fe.author(name=tweet['user']['name'])
@@ -58,6 +64,7 @@ def create_rss_feed_xml():
     cached_output = output
     return output
 
+
 @bottle.route('/')
 @bottle.route('/feed.xml')
 def feed_xml():
@@ -65,8 +72,10 @@ def feed_xml():
     bottle.response.content_type = 'application/xml'
     return data
 
+
 def main():
     bottle.run(host='0.0.0.0', port=8081)
+
 
 if __name__ == '__main__':
     main()
